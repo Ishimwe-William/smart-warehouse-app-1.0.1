670e0d3556a7e3e98ba6fe23341c31e73d2d003e
Ishimwe-William <ishimwe.william2000@gmail.com>
2024-12-23 08:22:36 +0200

    fixed: Warning: React has detected a change in the order of Hooks called by StackNavigator.

--- src/utils/headerOptions.js
+++ src/utils/headerOptions.js
@@ -1,50 +1,55 @@
+// headerOptions.js
+import React from 'react';
 import {TouchableOpacity, View} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
-import React from "react";
 import {useNotification} from "../context/NotificationContext";
 
-export const defaultHeaderOptions = ({navigation, route}) => {
+// Create a separate component for the notification button
+const NotificationButton = ({navigation}) => {
     const {unreadNotifications} = useNotification();
 
     const handleNotPress = () => {
         navigation.navigate("NotificationsScreen");
     };
 
-    return {
-        headerTitle: route.name,
-        headerStyle: {
-            backgroundColor: "#fff",
-            borderBottomWidth: 3,
-        },
-        headerTitleAlign: "center",
-        headerRight: () => (
-            <TouchableOpacity
-                onPress={handleNotPress}
-                style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    marginHorizontal: 20,
-                }}
-            >
-                <View>
-                    <Ionicons name="notifications-outline" size={24} color="black"/>
-                    {unreadNotifications && (
-                        <View
-                            style={{
-                                position: "absolute",
-                                right: -3,
-                                top: -3,
-                                backgroundColor: "red",
-                                borderRadius: 6,
-                                width: 10,
-                                height: 10,
-                                justifyContent: "center",
-                                alignItems: "center",
-                            }}
-                        />
-                    )}
-                </View>
-            </TouchableOpacity>
-        ),
-    };
+    return (
+        <TouchableOpacity
+            onPress={handleNotPress}
+            style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginHorizontal: 20,
+            }}
+        >
+            <View>
+                <Ionicons name="notifications-outline" size={24} color="black"/>
+                {unreadNotifications && (
+                    <View
+                        style={{
+                            position: "absolute",
+                            right: -3,
+                            top: -3,
+                            backgroundColor: "red",
+                            borderRadius: 6,
+                            width: 10,
+                            height: 10,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    />
+                )}
+            </View>
+        </TouchableOpacity>
+    );
 };
+
+// Modify the header options to use the component
+export const defaultHeaderOptions = ({navigation, route}) => ({
+    headerTitle: route.name,
+    headerStyle: {
+        backgroundColor: "#fff",
+        borderBottomWidth: 3,
+    },
+    headerTitleAlign: "center",
+    headerRight: () => <NotificationButton navigation={navigation}/>,
+});
